6e62fc5fdb026e91797904170d570d607d747d2a
Wesley Vila Seca Sanches <wesley.vilaseca@gmail.com>
2021-11-06 21:08:21 -0300

    componentisando o dashboard

---
+++ src/components/Layout/Dashboard/DashboardComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <body>
+      <TopBarComponent />
+    <!-- offcanvas -->
+    <SideBarComponent />
+    <!-- offcanvas -->
+    <main class="mt-5 pt-3">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <slot />
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</template>
+
+<script>
+import TopBarComponent from './TopBarComponent.vue'
+import SideBarComponent from './SideBarComponent.vue'
+
+export default {
+  components: {
+    TopBarComponent,
+    SideBarComponent
+  },
+  name: "DashBoardComponent",
+};
+</script>
