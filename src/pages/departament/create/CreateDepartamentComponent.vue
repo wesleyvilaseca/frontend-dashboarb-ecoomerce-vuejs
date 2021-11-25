87b00e0c72068b013bd8dcbc8f7967652c2fb9f9
Wesley Vila Seca Sanches <wesley.vilaseca@gmail.com>
2021-11-24 21:35:37 -0300

    crud para a criação de novos departamentos

--- src/pages/departament/create/CreateDepartamentComponent.vue
+++ src/pages/departament/create/CreateDepartamentComponent.vue
@@ -3,33 +3,78 @@
     <template v-slot:page-content>
       <div class="contatiner d-flex justify-content-between">
         <h4>Departamento</h4>
-        <button class="btn btn-sm btn-primary" @click="back">
+        <router-link
+          class="btn btn-sm btn-primary"
+          :to="{ name: 'admin-departament' }"
+        >
           <i class="fas fa-arrow-left"></i> Voltar
-        </button>
+        </router-link>
       </div>
 
       <div class="container-fluid">
-        <form>
+        <form @submit.stop.prevent="save">
           <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label"
               >Nome do departamento</label
             >
-            <input type="email" class="form-control" v-model="name" />
+            <input type="text" class="form-control" v-model="name" />
           </div>
-          <div class="mb-3 position-relative">
+          <div
+            class="mb-3 position-relative"
+            v-if="departaments_selected.length <= 0"
+          >
             <label for="exampleInputPassword1" class="form-label"
               >Departamento pai</label
             >
-            <input type="text" class="form-control" v-model="search" autocomplete="off" @keyup="searchDepartament" />
+            <input
+              type="text"
+              class="form-control"
+              v-model="search"
+              autocomplete="off"
+              @keyup="searchDepartament"
+            />
 
-            <div v-if="departaments.length > 0" class="position-absolute drop-list">
+            <div
+              v-if="departaments.length > 0"
+              class="position-absolute drop-list"
+            >
               <div class="list">
                 <ul>
-                  <li class="d-block">item 1</li>
+                  <li
+                    class="d-block"
+                    v-for="(departament, index) in departaments"
+                    :key="index"
+                    @click="selectDepartament(departament)"
+                  >
+                    {{ departament.name }}
+                  </li>
                 </ul>
               </div>
             </div>
           </div>
+
+          <div v-if="departaments_selected.length > 0">
+            <div class="card mt-2 mb-2">
+              <div class="card-header">Departamento selecionado</div>
+              <div class="card-body">
+                <ul class="list-group list-group-flush">
+                  <li
+                    class="list-group-item"
+                    v-for="(departament, index) in departaments_selected"
+                    :key="index"
+                  >
+                    {{ departament.name }}
+                    <span
+                      class="ms-5 fs-5 text-danger"
+                      @click="unselectDepartament(index)"
+                      ><i class="fas fa-times"></i
+                    ></span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
           <button type="submit" class="btn btn-primary">Salvar</button>
         </form>
       </div>
@@ -49,27 +94,57 @@ export default {
       search: "",
       departaments: [],
       departaments_selected: [],
-      
     };
   },
   methods: {
-    sendEvent() {
-      const event = new CustomEvent("catalog");
-      window.dispatchEvent(event);
+    async searchDepartament() {
+      if (this.search == "") {
+        return (this.departaments = []);
+      }
+
+      try {
+        const { data } = await this.$http.get(
+          "admin-departament/search?name=" + this.search
+        );
+        return (this.departaments = data);
+      } catch (error) {
+        console.log(error);
+      }
     },
 
-
-    searchDepartament(){
-      console.log(this.search);
+    selectDepartament(departament) {
+      this.departaments = [];
+      this.search = "";
+      this.departaments_selected.push(departament);
     },
 
+    unselectDepartament(index) {
+      this.departaments_selected.splice(index, 1);
+      // const filter = this.departaments_selected.filter(element => element !== departament);
+      // this.departaments_selected = filter;
+    },
 
-    back() {
-      return this.$router.push({ name: "admin-departament" });
+    async save() {
+      try {
+        const { data } = await this.$http.post("admin-departament/save", {
+          name: this.name,
+          parent_id: this.departaments_selected
+        });
+
+        this.$toast.success(`${data.msg}`, {  position: "top" });
+
+        this.$router.push({name: 'admin-departament'});
+        
+      } catch (error) {
+        const { data } = error;
+        this.$toast.error(`${data.error}`, {  position: "top" });
+      }
     },
   },
   mounted() {
-    this.sendEvent();
+    const event = new CustomEvent("catalog");
+    window.dispatchEvent(event);
   },
+  watch: {},
 };
 </script>
